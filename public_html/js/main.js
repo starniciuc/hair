12b9976001012753707dde567a74043a160033f1
mstarniciuc <mstarniciuc@mstarniciuc-System-Product-Name>
2014-07-17 14:21:57 +0300

    collection

--- public_html/js/main.js
+++ public_html/js/main.js
@@ -1,1 +1,8 @@
 
+$(".show-more").click(function() {
+    var mybtn = $(this);
+    var par = mybtn.parents(".colect-par").children(".col-sm-11").children(".image").children(".collection-hidden");
+	$(mybtn).toggleClass("active");
+	par.toggle("fast");
+
+});
